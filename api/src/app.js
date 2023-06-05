8a60ce42cafe7de01c17fdf3120aaef2cef5a7ea
sofia perone <sofiaperone62@gmail.com>
2023-06-05 04:10:15 -0300

    iniciando repo

---
+++ api/src/app.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',      // Dirección del servidor MySQL
+    user: 'sofiaperone1',        // Nombre de usuario
+    password: 'sofiaperone1', // Contraseña
+    database: 'Teddy'   // Nombre de la base de datos
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error al conectar a la base de datos:', err);
+      return;
+    }
+    console.log('Conexión exitosa a la base de datos MySQL');
+  });
+
+  connection.end();
